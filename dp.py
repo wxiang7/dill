4d847fca5b6d55e53f95c67d9b1d9a6c4d1cd018
Wu Xiang <wu.xiang@hulu.com>
2015-11-03 11:48:45 +0800

    no-ticket: remove test files

--- dp.py
+++
@@ -1,9 +0,0 @@
-import imp
-import dill.dill.dill
-import sys
-
-sys.path.append('/Vault/workspace/github/airflow/dags')
-
-m = imp.load_source('tset_mod', '/Vault/workspace/github/airflow/dags/testdruid.py')
-
-print(dill.dill.dumps(m.dag, main=m))
